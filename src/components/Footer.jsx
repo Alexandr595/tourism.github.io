b9291cc6a1f65ccf98cf177b4d38bf934c84e3a7
Alexandr595 <108999258+Alexandr595@users.noreply.github.com>
2023-05-28 11:11:00 +0500

    Initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        <footer className='pt-4 my-md-5 border-top'>
-
-        </footer>
+    <div className='container'>
+        
     </div>
   )
 }
